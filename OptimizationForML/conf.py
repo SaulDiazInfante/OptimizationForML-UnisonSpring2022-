146ff1d418626435ec4f8df8d3eec43c39c24abc
SAUL DIAZ INFANTE VELASCO <sauld@cimat.mx>
2022-01-02 21:05:45 -0700

    Commit from Pycharm

---
+++ OptimizationForML/conf.py
@@ -0,0 +1,14 @@
+myst_enable_extensions = [
+    "amsmath",
+    "colon_fence",
+    "deflist",
+    "dollarmath",
+    "fieldlist",
+    "html_admonition",
+    "html_image",
+    "linkify",
+    "replacements",
+    "smartquotes",
+    "substitution",
+    "tasklist",
+]
